faebc82eb16f214b663a6c75cf4c51bf44c1b767
Youfa Wang <wangy332@myunitec.ac.nz>
2017-11-02 12:25:38 +1300

    up

--- DiscountNZ/app/src/main/java/com/discountnz/android/discountnz/MainActivity.java
+++ DiscountNZ/app/src/main/java/com/discountnz/android/discountnz/MainActivity.java
@@ -61,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button viewButton = (Button) findViewById(R.id.viewButton);
